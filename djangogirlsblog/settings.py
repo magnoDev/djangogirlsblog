aa52becbbac2cac47c49f379021339a3e7270fa9
Magno Macedo de Oliveira <magno_macedo2099@hotmail.com>
2020-05-08 11:57:12 -0300

    refactor(Ajustes-no-projeto): Criação do arquivo .gitignore e criação da tabela no banco de dados

--- djangogirlsblog/settings.py
+++ djangogirlsblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-73%$r$+z&f63r)r0hdffjrqekmrmx6gjk_pqm#d)w*c106-)f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
